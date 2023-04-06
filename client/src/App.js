b12053fcd0720d879f58521479d65e40e878eac0
Franco Inca <francoinca1604@gmail.com>
2023-04-06 00:17:28 -0500

    Optimized design for App.css due to Footer errors

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,23 @@ import Home from './Components/Home/Home';
 import NavBar from './Components/NavBar/NavBar';
 import Details from './Components/Details/Details';
 import NotFound from './Components/NotFound/NotFound';
+import Footbar from './Components/Footbar/Footbar';
+import FormActivities from './Components/FormActivities/FormActivities';
+import './app.css';
 
 function App() {
   return (
-    <>
+    <div className="App">
       <NavBar />
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/home/:page" element={<Home />} />
         <Route path="/details/:cc" element={<Details />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/publish" element={<FormActivities />} />
       </Routes>
-    </>
+      <Footbar />
+    </div>
   );
 }
 
