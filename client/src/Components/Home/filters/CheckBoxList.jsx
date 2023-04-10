32bcea0f0147cd8a95c29e117fc0b2dbeecf42a2
Franco Inca <francoinca1604@gmail.com>
2023-04-09 20:09:24 -0500

    Changed to more accurate variables

--- client/src/Components/Home/filters/CheckBoxList.jsx
+++ client/src/Components/Home/filters/CheckBoxList.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Styles from '../Home.module.css';
 
-const CheckBoxList = ({continents, functionHandle, filters, name}) => {
+const CheckBoxList = ({list, functionHandle, filters, name}) => {
   return (
     <div className={Styles.checkbox_container}>
       <h3>{name}</h3>
       <div className={Styles.list}>
-        {continents.map((e) => (
+        {list.map((e) => (
           <div key={e}>
             <label htmlFor={e}>{e}</label>
             <input type="checkbox" id={e} value={e} onChange={(e) => functionHandle(e)} checked={filters.includes(e)} />
