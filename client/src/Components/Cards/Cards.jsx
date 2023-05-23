a2e1110fbc98a0a536fef74c1656670e73ed5f51
Franco Inca <francoinca1604@gmail.com>
2023-05-23 13:45:04 -0500

    fixed Cards showing 0 error

--- client/src/Components/Cards/Cards.jsx
+++ client/src/Components/Cards/Cards.jsx
@@ -4,7 +4,7 @@ import Card from './Card';
 const Cards = ({ props }) => {
   return (
     <ul>
-      {props.length && props.map((e) => (
+      {props.length >= 0 && props.map((e) => (
         <Card name={e.name} key={e.id} flag={e.flag} capital={e.capital} id={e.id} activities={e.Activities.length} />
       ))}
     </ul>
