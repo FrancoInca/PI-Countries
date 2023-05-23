bb8a0b3ac5368d0883134c295830780f2b55abe2
Franco Inca <francoinca1604@gmail.com>
2023-05-23 13:42:06 -0500

    Added PORT env variable

--- api/index.js
+++ api/index.js
@@ -21,12 +21,12 @@ require('dotenv').config();
 const server = require('./src/app.js');
 const { addCountries } = require('./src/functions/loadCountries.js');
 const { sequelize } = require('./src/db.js');
-const { DB_PORT } = process.env;
+const { PORT } = process.env;
 
 // Syncing all the models at once.
-server.listen(DB_PORT, async () => {
+server.listen(PORT, async () => {
   await sequelize.sync({ force: true }); //Solo para pruebas, descomentar el de abajo para el deploy y evitar la perdida de datos de la DB
   //await sequelize.sync()
-  console.log(`Server listening at ${DB_PORT}`);
+  console.log(`Server listening at ${PORT}`);
   await addCountries(); //Comentar al momento del deploy, funcion para cargar los datos sin el Bulk
 });
