2d9c4874151d150751f71d8e83c25e62e5f97aa6
Franco Inca <francoinca1604@gmail.com>
2023-05-23 13:32:29 -0500

    fixed index

--- api/index.js
+++ api/index.js
@@ -17,14 +17,16 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+require('dotenv').config();
 const server = require('./src/app.js');
-const {addCountries} = require('./src/functions/loadCountries.js');
-const {sequelize} = require('./src/db.js');
+const { addCountries } = require('./src/functions/loadCountries.js');
+const { sequelize } = require('./src/db.js');
+const { DB_PORT } = process.env;
 
 // Syncing all the models at once.
-server.listen(3001, async () => {
-  await sequelize.sync({force: true}); //Solo para pruebas, descomentar el de abajo para el deploy y evitar la perdida de datos de la DB
+server.listen(DB_PORT, async () => {
+  await sequelize.sync({ force: true }); //Solo para pruebas, descomentar el de abajo para el deploy y evitar la perdida de datos de la DB
   //await sequelize.sync()
-  console.log('listening at 3001');
+  console.log(`Server listening at ${DB_PORT}`);
   await addCountries(); //Comentar al momento del deploy, funcion para cargar los datos sin el Bulk
 });
